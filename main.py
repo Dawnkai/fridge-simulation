551ab0206829a1009860a46658fcf21b795946a4
Maciej Kleban <dawnwender@gmail.com>
2023-04-09 00:22:13 +0200

    Display 8 latest results on one graph

--- main.py
+++ main.py
@@ -1,113 +1,57 @@
 import dash_bootstrap_components as dbc
-from dash import Dash, Input, Output, State, dcc, html
-from controls import get_controls, get_result_graphs
+from dash import Dash, Input, Output, State
+from utils import get_controls, get_result_graphs, get_app_layout
 
 from simulation import Simulation
 from pi_regulator import PI_Regulator
 from pid_regulator import PID_Regulator
 
-simulation = Simulation()
+class Display:
+    def __init__(self):
+        self.simulation = Simulation()
+        self.results = []
+        self.app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+        self.app.css.config.serve_locally = True
+        self.app.scripts.config.serve_locally = True
+        self.app.layout = get_app_layout()
+        self.app.callback(
+        [
+            Output("simulation-result", "children"),
+            Output("controls", "children")
+        ],
+        [
+            Input("regulator-type", "value"),
+            State("target-value", "value"),
+            State("pi-proportional", "value"),
+            State("pi-integral", "value"),
+            State("pid-proportional", "value"),
+            State("pid-integral", "value"),
+            State("pid-derivative", "value"),
+            Input("start-simulation", "n_clicks")
+        ])(self.make_graph)
 
-app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.css.config.serve_locally = True
-app.scripts.config.serve_locally = True
+    def start(self):
+        self.app.run_server()
 
-app.layout = dbc.Container(
-    [
-        dbc.Card([
-            html.H1("Levitating Ball Simulation", style={"textAlign": "center", "color": "white"}),
-            html.Hr(),
-            dbc.Row(
-                [
-                    # Window with parameters
-                    dbc.Col(
-                        dbc.Card(
-                            [
-                                html.Div(
-                                    [
-                                        dbc.Label("Regulator type", style={"fontWeight": "bold"}),
-                                        dcc.Dropdown(
-                                            id="regulator-type",
-                                            options=[
-                                                {"label": "PI", "value": "PI"},
-                                                {"label": "PID", "value": "PID"}
-                                            ],
-                                            value="PI",
-                                        )
-                                    ]
-                                ),
-                                html.Div(
-                                    [
-                                        dbc.Label("Target value", style={'marginTop': 10, "fontWeight": "bold"}),
-                                        html.Br(),
-                                        dcc.Input(
-                                            type="number",
-                                            id="target-value",
-                                            step=1,
-                                            value=40
-                                        )
-                                    ]
-                                ),
-                                html.Hr(),
-                                html.Div(
-                                    dbc.Card(
-                                        get_controls("PI"),
-                                        body=True,
-                                        id="controls"
-                                    )
-                                ),
-                                dbc.Button("Go", color="primary", id="start-simulation", n_clicks=0, style={'marginTop': 15, 'width': '100%'})
-                            ],
-                            body=True,
-                        )
-                    , md=4),
-                    # Plots and results
-                    dbc.Col(html.Div(
-                        dbc.Tabs(),
-                        id="simulation-result"
-                    ),md=8)
-                ],
-                align="center",
-            )],
-            body=True,
-            style={"height": "100%", "marginTop": 15, "marginBottom": 15, "backgroundColor": "#295e8f"}
-        )
-    ],
-    fluid=True
-)
+    def make_graph(self, regulator_type, target_value, pi_p, pi_i, pid_p, pid_i, pid_d, _):
+        if regulator_type != self.simulation.get_regulator_type():
+            if regulator_type == "PI":
+                self.simulation.set_regulator(PI_Regulator())
+            else:
+                self.simulation.set_regulator(PID_Regulator())
 
-@app.callback(
-    [
-        Output("simulation-result", "children"),
-        Output("controls", "children")
-    ],
-    [
-        Input("regulator-type", "value"),
-        State("target-value", "value"),
-        State("pi-proportional", "value"),
-        State("pi-integral", "value"),
-        State("pid-proportional", "value"),
-        State("pid-integral", "value"),
-        State("pid-derivative", "value"),
-        Input("start-simulation", "n_clicks")
-    ],
-)
-def make_graph(regulator_type, target_value, pi_p, pi_i, pid_p, pid_i, pid_d, n_clicks):
-    if regulator_type != simulation.get_regulator_type():
         if regulator_type == "PI":
-            simulation.set_regulator(PI_Regulator())
+            self.simulation.reset_regulator(target_value, pi_p, pi_i)
         else:
-            simulation.set_regulator(PID_Regulator())
+            self.simulation.reset_regulator(target_value, pid_p, pid_i, pid_d)
 
-    if regulator_type == "PI":
-        simulation.reset_regulator(target_value, pi_p, pi_i)
-    else:
-        simulation.reset_regulator(target_value, pid_p, pid_i, pid_d)
-
-    simulation.reset()
-    simulation.start()
-    time, velocity, _, signal, error = simulation.get_display_results()
-    return get_result_graphs(time, velocity, signal, error), get_controls(regulator_type, pi_p, pi_i, pid_p, pid_i, pid_d)
+        self.simulation.reset()
+        self.simulation.start()
+        if len(self.results) > 8:
+            self.results.pop(0)
+        self.results.append(self.simulation.get_display_results())
+        return get_result_graphs(self.results), get_controls(regulator_type, pi_p, pi_i, pid_p, pid_i, pid_d)
 
 if __name__ == "__main__":
-    app.run_server()
+    disp = Display()
+    disp.start()
