cfaf9652091584da8344f590d9a39537e5c8b3c4
DawnKai <dawnwender@gmail.com>
2023-05-02 22:34:02 +0200

    Remove functions for basic selects and inserts

--- database.py
+++ database.py
@@ -16,39 +16,19 @@ def get_connection(dbpath : str):
         print(f"Sqlite error in get_connection: {error}")
     return conn
 
-def execute_query(conn, query : str, commit : bool) -> bool:
+def create_tables(conn):
     '''
-    Run SQL query.
+    Create required sqlite3 tables.
     :param conn: database connection object.
-    :param query: SQL query to execute.
-    :param commit: whether to run commit after execution or not.
     '''
     try:
         cur = conn.cursor()
-        cur.execute(query)
-        cur.close()
-        if commit:
-            conn.commit()
-    except sqlite3.Error as error:
-        print(f"Sqlite error when executing query ({query}): {error}")
-        return False
-    return True
-
-def select_query(conn, query : str) -> list:
-    '''
-    Run SELECT query and return result as list of tuples.
-    :param conn: database connection object.
-    :param query: SQL SELECT query to execute.
-    '''
-    rows = []
-    try:
-        cur = conn.cursor()
-        cur.execute(query)
-        rows = cur.fetchall()
+        with open("database_script.sql") as script:
+            cur.executescript(script.read())
+        conn.commit()
         cur.close()
     except sqlite3.Error as error:
-        print(f"Sqlite error when executing query ({query}): {error}")
-    return rows
+        print(f"Erron in create_tables: {error}")
 
 def get_new_simulation_id(conn) -> int:
     '''
@@ -56,15 +36,19 @@ def get_new_simulation_id(conn) -> int:
     :param conn: database connection object.
     '''
     try:
-        rows = select_query(conn, "SELECT MAX(simulation_id) FROM Simulations")
+        cur = conn.cursor()
+        cur.execute("SELECT MAX(simulation_id) + 1 FROM Simulations")
+        rows = cur.fetchall()
         if len(rows) > 0 and rows[0][0] is not None:
             return rows[0][0] + 1
         return 0
     except sqlite3.Error as error:
         print(f"Sqlite error in get_new_simulation_id: {error}")
+    finally:
+        cur.close()
     return -1
 
-def insert_simulation(conn, sim_id : int, param_1 : float, param_2 : float, param_3 : float,
+def insert_simulation(conn, sim_id : int, param_1, param_2, param_3,
                       target_value : float, regulator_type : str) -> bool:
     '''
     Insert simulation data into database.
@@ -77,14 +61,28 @@ def insert_simulation(conn, sim_id : int, param_1 : float, param_2 : float, para
     :target_value: target value of the simulation.
     :regulator_type: regulator used in simulation.
     '''
-    insert_query = (
-        f"INSERT INTO Simulations (simulation_id, date, time, param_1, param_2, "
-        f"param_3, target_value, regulator_type) VALUES ({sim_id}, "
-    )
     now = datetime.now()
-    insert_query += f"{now.strftime('%Y%m%d')}, {now.strftime('%H%M%S')},"
-    insert_query += f"{param_1}, {param_2}, {param_3}, {target_value}, '{regulator_type}')"
-    return execute_query(conn, insert_query, True)
+    try:
+        cur = conn.cursor()
+        cur.execute(
+            f"INSERT INTO Simulations"
+            f"(simulation_id, simulation_date, simulation_time, param_1, "
+            f"param_2,param_3, target_value, regulator_type)"
+            f"VALUES ("
+            f"{sim_id}, {now.strftime('%Y%m%d')}, {now.strftime('%H%M%S')},"
+            f"{param_1 if param_1 is not None else 'NULL'},"
+            f"{param_2 if param_2 is not None else 'NULL'},"
+            f"{param_3 if param_3 is not None else 'NULL'},"
+            f"{target_value}, '{regulator_type}'"
+            f")"
+        )
+        conn.commit()
+    except sqlite3.Error as error:
+        print(f"Error in insert_simulation: {error}")
+        return False
+    finally:
+        cur.close()
+    return True
 
 def insert_measurements(conn, sim_id : int, measurements : list) -> bool:
     '''
@@ -92,15 +90,26 @@ def insert_measurements(conn, sim_id : int, measurements : list) -> bool:
     :param conn: database connection object.
     :param measurements: list of simulation display results.
     '''
-    insert_query = "INSERT INTO Measurements (simulation_id, time, signal, signal_response, error) VALUES "
+    insert_query = ("INSERT INTO Measurements "
+                    "(simulation_id, measurement_time, measurement_value, measurement_signal,"
+                    "measurement_response, error) VALUES ")
     for idx, time_measurement in enumerate(measurements[0]):
-        insert_query += f"""({sim_id}, {time_measurement}, {measurements[2][idx]},
+        insert_query += f"""({sim_id}, {time_measurement}, {measurements[1][idx]}, {measurements[2][idx]},
                          {measurements[3][idx]}, {measurements[4][idx]}), """
     insert_query = insert_query[:-2]
-    return execute_query(conn, insert_query, True)
+    try:
+        cur = conn.cursor()
+        cur.execute(insert_query)
+        conn.commit()
+    except sqlite3.Error as error:
+        print(f"Error in insert_measurements: {error}")
+        return False
+    finally:
+        cur.close()
+    return True
 
-def check_simulation_exists(conn, pi_p : float, pi_i : float, pid_p : float, pid_i : float,
-                            pid_d : float, target_value : float, regulator_type : str) -> bool:
+def simulation_exists(conn, pi_p : float, pi_i : float, pid_p : float, pid_i : float,
+                      pid_d : float, target_value : float, regulator_type : str) -> bool:
     '''
     Check if simulation using provided parameters already exists in the database.
     :param conn: database connection object.
@@ -127,10 +136,16 @@ def check_simulation_exists(conn, pi_p : float, pi_i : float, pid_p : float, pid
         test_query += "SELECT * FROM Simulations WHERE "
     test_query += f"target_value = {float(target_value)} AND regulator_type = '{regulator_type}'"
 
-    simulation_check = select_query(conn, test_query)
-    if simulation_check != []:
-        return True
-    return False
+    try:
+        cur = conn.cursor()
+        cur.execute(test_query)
+        if cur.fetchall() != []:
+            return True
+    except sqlite3.Error as error:
+        print(f"Error in simulation_exists: {error}")
+        return False
+    finally:
+        cur.close()
 
 def insert_data(conn, pi_p : float, pi_i : float, pid_p : float, pid_i : float, pid_d : float,
                 target_value : float, regulator_type : str, measurements : list) -> bool:
@@ -149,11 +164,11 @@ def insert_data(conn, pi_p : float, pi_i : float, pid_p : float, pid_i : float, 
     new_sim_id = get_new_simulation_id(conn)
     success = True
     if regulator_type == "PI":
-        success = insert_simulation(conn, new_sim_id, pi_p, pi_i, "NULL", target_value, "PI")
+        success = insert_simulation(conn, new_sim_id, pi_p, pi_i, None, target_value, "PI")
     elif regulator_type == "PID":
         success = insert_simulation(conn, new_sim_id, pid_p, pid_i, pid_d, target_value, "PID")
     else:
-        success = insert_simulation(conn, new_sim_id, "NULL", "NULL", "NULL", target_value, "Fuzzy")
+        success = insert_simulation(conn, new_sim_id, None, None, None, target_value, "Fuzzy")
     return success and insert_measurements(conn, new_sim_id, measurements)
 
 def get_latest_simulations(conn, n_simulations : int):
@@ -162,24 +177,35 @@ def get_latest_simulations(conn, n_simulations : int):
     :param conn: database connection object.
     :param n_simulations: number of simulations to fetch.
     '''
-    simulations = select_query(conn,
-                               f"SELECT simulation_id FROM Simulations ORDER BY date, time DESC LIMIT {n_simulations}")
-    sim_ids = [ val[0] for val in simulations ]
-    if sim_ids == [None]:
-        return None
+    try:
+        cur = conn.cursor()
+        cur.execute(
+            f"SELECT simulation_id FROM Simulations "
+            f"ORDER BY simulation_date, simulation_time DESC LIMIT {n_simulations}"
+        )
+        sim_ids = [ val[0] for val in cur.fetchall() ]
 
-    results = []
-    for sim_id in sim_ids:
-        measurements = select_query(conn,
-                                    (
-                                        f"SELECT time, signal, signal_response, error FROM Measurements "
-                                        f"WHERE simulation_id = {sim_id} ORDER BY time"
-                                    ))
-        parsed_measurements = [[], [], [], [], []]
-        for measure in measurements:
-            parsed_measurements[0].append(measure[0])
-            parsed_measurements[2].append(measure[1])
-            parsed_measurements[3].append(measure[2])
-            parsed_measurements[4].append(measure[3])
-        results.append(parsed_measurements)
-    return results
+        if sim_ids == [None]:
+            return None
+
+        results = []
+        for sim_id in sim_ids:
+            cur.execute(
+                f"SELECT measurement_time, measurement_value, measurement_signal, measurement_response,"
+                f"error FROM Measurements "
+                f"WHERE simulation_id = {sim_id} ORDER BY measurement_time"
+            )
+            parsed_measurements = [[], [], [], [], []]
+            for measure in cur.fetchall():
+                parsed_measurements[0].append(measure[0])
+                parsed_measurements[1].append(measure[1])
+                parsed_measurements[2].append(measure[2])
+                parsed_measurements[3].append(measure[3])
+                parsed_measurements[4].append(measure[4])
+            results.append(parsed_measurements)
+        return results
+    except sqlite3.Error as error:
+        print(f"Error in get_latest_simulations: {error}")
+        return []
+    finally:
+        cur.close()
